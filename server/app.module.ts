1eb249c083fb01f758bd3bf1b619147142438286
Tyler <tyty5949@gmail.com>
2020-07-16 20:15:56 -0500

    Add database scaffolding

--- server/app.module.ts
+++ server/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      // NOTE: Most configuration is done using environment variables
+      // @see https://typeorm.io/#/using-ormconfig/using-environment-variables
+      entities: ['domain/**/*.entity.js'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/main.ts
+++ server/main.ts
@@ -2,8 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+  }
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.NEST_PORT);
 }
 
-bootstrap().then(() => console.log('Started server!'));
+bootstrap();
